1bbf288ee0b434a8fa3e40b51254b15ca586d71f
Noemi Soares <142548973+noemisoares@users.noreply.github.com>
2025-09-09 21:48:53 +0000

    Atualização de estrutura de conteiner de page

--- my-portfolio/components/Footer.js
+++ my-portfolio/components/Footer.js
@@ -4,10 +4,8 @@ import { Social } from "./Social";
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <p> Recife, PE</p>
-      <p> (81) 98333-6777</p>
       <Social className={styles.socialsFooter} />
-
+      <p>Recife, PE</p>
       <p>&copy; {new Date().getFullYear()} Noemi Soares</p>
     </footer>
   );
